64d8627c2021e676f15f285db2d5db099429d6ed
Pragya <prag0023@stud.kea.dk>
2021-02-13 10:26:39 +0100

    Changes made

---
+++ 2/0._Misc/1._Intro_Exercises/3.Objects.js
@@ -0,0 +1,45 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = {"message": "Hello, earthling! I bring peace."};
+
+console.log(myObj.message);
+
+// Log the message 
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+ const object1 = { 
+   name:"Pragya",
+   age :21
+ };
+
+ console.log(object1.name + "" + object1.age) ;
+
+ 
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+ stackOverflow.isAllowed = true;
+
+  // make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = {"description": "The best song in the world."}
+delete thisSong.description;
+
+thisSong.about="Just a tribute";
+console.log(thisSong);
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+
+// --------------------------------------
+
+
